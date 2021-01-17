276aa7e675cc1da8a66216bbf10edd48eae915ff
B M <adjunctfuentes@gmail.com>
2021-01-16 21:51:16 -0600

    first kf working files

---
+++ katy_working_folder/Readme_draft.md
@@ -0,0 +1,32 @@
+# 2020 S&P100 Stock Price Predictions from Earnings Calls
+
+Rice Data Analytics Program
+Team Members: Radhika Balasubramaniam, Chad Dubiel, Katy Fuentes, Pankaj Tahiliani
+
+## Key Considerations
+Is there a change in price in stock after an earnings call?
+What are common words used across various S&P 100 company earnings calls?
+Can certain words influence the stock price?
+What is the percentage change for each before and after the call?
+
+
+### Model 1
+
+
+### Model 2
+
+
+### Model 3
+
+
+## Conclusion
+
+
+#### Data Sources
+
+S&P 100 List https://en.wikipedia.org/wiki/List_of_stock_exchanges 
+Earnings Calls Transcripts* https://seekingalpha.com/earnings/earnings-call-transcripts 
+*The transcripts from Seeking Alpha are protected by copyright and cannot be used for commercial purposes. This is an educational project for the Data Visualization Program at Rice University and the use of the information should be permitted on the Copyright Fair Use principal.  
+Yahoo Finance Historical stock price- https://sg.finance.yahoo.com/ 
+
+
---
+++ katy_working_folder/sql_drafts/QuickDBD-Final_project.sql
@@ -0,0 +1,40 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/m2mvF9
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+SET XACT_ABORT ON
+
+BEGIN TRANSACTION QUICKDBD
+
+CREATE TABLE [stock_data] (
+    [Symbol] string,  NOT NULL ,
+    [Name] string  NOT NULL ,
+    [Date] date  NOT NULL ,
+    [Adj_close] int  NOT NULL ,
+    [Close] int  NOT NULL ,
+    [High] int  NOT NULL ,
+    [Low] int  NOT NULL ,
+    [Open] int  NOT NULL ,
+    [Volume] int  NOT NULL ,
+    CONSTRAINT [PK_stock_data] PRIMARY KEY CLUSTERED (
+        [Symbol] ASC
+    )
+)
+
+CREATE TABLE [stock_transcript_data] (
+    [Symbol] string  NOT NULL ,
+    [Name] string  NOT NULL ,
+    [Date] date  NOT NULL ,
+    [Transcript] string  NOT NULL ,
+    CONSTRAINT [PK_stock_transcript_data] PRIMARY KEY CLUSTERED (
+        [Symbol] ASC
+    )
+)
+
+ALTER TABLE [stock_data] WITH CHECK ADD CONSTRAINT [FK_stock_data_Symbol] FOREIGN KEY([Symbol])
+REFERENCES [stock_transcript_data] ([Symbol])
+
+ALTER TABLE [stock_data] CHECK CONSTRAINT [FK_stock_data_Symbol]
+
+COMMIT TRANSACTION QUICKDBD
