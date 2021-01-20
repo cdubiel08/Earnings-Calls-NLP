d589dfb85b665de4be1c06ce19a73ca068d4fda0
B M <adjunctfuentes@gmail.com>
2021-01-19 21:38:01 -0600

    updated stock list for 2015-2020 pricing

--- katy_working_folder/sql_drafts/QuickDBD-Final_project.sql
+++ katy_working_folder/sql_drafts/QuickDBD-Final_project.sql
@@ -3,48 +3,62 @@
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 
-CREATE TABLE "stock_price_data" (
-    "Symbol" text,   NOT NULL,
-    "Name" text   NOT NULL,
-    "Date" text   NOT NULL,
-    "Adj_close" int   NOT NULL,
-    "Close" int   NOT NULL,
-    "High" int   NOT NULL,
-    "Low" int   NOT NULL,
-    "Open" int   NOT NULL,
-    "Volume" text   NOT NULL,
-    CONSTRAINT "pk_stock_price_data" PRIMARY KEY (
-        "Symbol"
-     )
-);
+SET XACT_ABORT ON
 
-CREATE TABLE "stock_transcript_data" (
-    "Title" text   NOT NULL,
-    "URL" text   NOT NULL,
-    "Date" text   NOT NULL,
-    "Symbol" text   NOT NULL,
-    CONSTRAINT "pk_stock_transcript_data" PRIMARY KEY (
-        "Symbol"
-     )
-);
+BEGIN TRANSACTION QUICKDBD
 
-CREATE TABLE "stock_detail_data" (
-    "Symbol" text   NOT NULL,
-    "Name" text   NOT NULL,
-    "GICS_Sector" text   NOT NULL,
-    "GICS_SubIndustry" text   NOT NULL,
-    "Headquarters_Location" text   NOT NULL,
-    "Date_first_added" varchar   NOT NULL,
-    "CIK" int   NOT NULL,
-    "Founded" text   NOT NULL,
-    CONSTRAINT "pk_stock_detail_data" PRIMARY KEY (
-        "Symbol"
-     )
-);
+CREATE TABLE [stock_price_data] (
+    [stock_price_id] int  NOT NULL ,
+    [stock_id] int  NOT NULL ,
+    [Symbol] text  NOT NULL ,
+    [Name] text  NOT NULL ,
+    [Date] text  NOT NULL ,
+    [Adj_close] int  NOT NULL ,
+    [Close] int  NOT NULL ,
+    [High] int  NOT NULL ,
+    [Low] int  NOT NULL ,
+    [Open] int  NOT NULL ,
+    [Volume] text  NOT NULL ,
+    CONSTRAINT [PK_stock_price_data] PRIMARY KEY CLUSTERED (
+        [stock_price_id] ASC
+    )
+)
 
-ALTER TABLE "stock_price_data" ADD CONSTRAINT "fk_stock_price_data_Symbol" FOREIGN KEY("Symbol")
-REFERENCES "stock_transcript_data" ("Symbol");
+CREATE TABLE [stock_transcript_data] (
+    [stock_trans_id] int  NOT NULL ,
+    [stock_id] int  NOT NULL ,
+    [Title] text  NOT NULL ,
+    [URL] text  NOT NULL ,
+    [Date] text  NOT NULL ,
+    [Symbol] text  NOT NULL ,
+    CONSTRAINT [PK_stock_transcript_data] PRIMARY KEY CLUSTERED (
+        [stock_trans_id] ASC
+    )
+)
 
-ALTER TABLE "stock_detail_data" ADD CONSTRAINT "fk_stock_detail_data_Symbol" FOREIGN KEY("Symbol")
-REFERENCES "stock_transcript_data" ("Symbol");
+CREATE TABLE [stock_detail_data] (
+    [stock_id] int  NOT NULL ,
+    [Symbol] text  NOT NULL ,
+    [Name] text  NOT NULL ,
+    [GICS_Sector] text  NOT NULL ,
+    [GICS_SubIndustry] text  NOT NULL ,
+    [Headquarters_Location] text  NOT NULL ,
+    [Date_first_added] varchar  NOT NULL ,
+    [CIK] int  NOT NULL ,
+    [Founded] text  NOT NULL ,
+    CONSTRAINT [PK_stock_detail_data] PRIMARY KEY CLUSTERED (
+        [stock_id] ASC
+    )
+)
 
+ALTER TABLE [stock_price_data] WITH CHECK ADD CONSTRAINT [FK_stock_price_data_stock_id] FOREIGN KEY([stock_id])
+REFERENCES [stock_detail_data] ([stock_id])
+
+ALTER TABLE [stock_price_data] CHECK CONSTRAINT [FK_stock_price_data_stock_id]
+
+ALTER TABLE [stock_transcript_data] WITH CHECK ADD CONSTRAINT [FK_stock_transcript_data_stock_id] FOREIGN KEY([stock_id])
+REFERENCES [stock_detail_data] ([stock_id])
+
+ALTER TABLE [stock_transcript_data] CHECK CONSTRAINT [FK_stock_transcript_data_stock_id]
+
+COMMIT TRANSACTION QUICKDBD
